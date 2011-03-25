e090244b1a7a6a2e33a60735814fe532970eb4d0
Francis M. Tyers <ftyers@prompsit.com>
2011-03-25 20:18:33 +0000

    add testvoc scripts

---
+++ dev/testvoc.sh
@@ -0,0 +1,2 @@
+sh inconsistency.sh is-sv > /tmp/is-sv.testvoc ; sh inconsistency-summary.sh /tmp/is-sv.testvoc is-sv 
+sh inconsistency.sh sv-is > /tmp/sv-is.testvoc ; sh inconsistency-summary.sh /tmp/sv-is.testvoc sv-is
