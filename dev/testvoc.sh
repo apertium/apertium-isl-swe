8da701359759a324333707464da859c01df5a450
Francis M. Tyers <ftyers@prompsit.com>
2013-08-24 01:06:37 +0000

    bas h

--- dev/testvoc.sh
+++ dev/testvoc.sh
@@ -1,2 +1,2 @@
-sh inconsistency.sh is-sv > /tmp/is-sv.testvoc ; sh inconsistency-summary.sh /tmp/is-sv.testvoc is-sv 
-sh inconsistency.sh sv-is > /tmp/sv-is.testvoc ; sh inconsistency-summary.sh /tmp/sv-is.testvoc sv-is
+bash inconsistency.sh is-sv > /tmp/is-sv.testvoc ; bash inconsistency-summary.sh /tmp/is-sv.testvoc is-sv 
+bash inconsistency.sh sv-is > /tmp/sv-is.testvoc ; bash inconsistency-summary.sh /tmp/sv-is.testvoc sv-is
