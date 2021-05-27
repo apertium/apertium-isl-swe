e9a0c7e7b9c87f7b6b19b4510eac07b1a1af42f6
Tino Didriksen <mail@tinodidriksen.com>
2021-05-27 11:51:43 +0200

    OFTC, http -> https, url fixups

--- wiki-tests.sh
+++ wiki-tests.sh
@@ -12,9 +12,9 @@ printf "Running $1-tests with mode \"$mode\""
 if [ "$4" == "update" ]; then
     printf " with updated tests..."
     TMPHTML=`mktemp -t tmp.$SRCLANG-html.XXXXXXXXXX`;
-    wget -O $TMPHTML -q http://wiki.apertium.org/wiki/Icelandic_and_Swedish/$TESTTYPE
+    wget -O $TMPHTML -q https://wiki.apertium.org/wiki/Icelandic_and_Swedish/$TESTTYPE
     if [[ -s $TMPHTML ]]; then mv $TMPHTML $HTML;
-    else rm $TMPHTML; echo "Couldn't fetch http://wiki.apertium.org/wiki/Icelandic_and_Swedish/$TESTTYPE"; fi
+    else rm $TMPHTML; echo "Couldn't fetch https://wiki.apertium.org/wiki/Icelandic_and_Swedish/$TESTTYPE"; fi
 fi
 echo "..."
 
